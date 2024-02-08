3c300b616b26c618e0ec2c6343f2f5bfa632b275
Catia Antunes <antunes.csr@gmail.com>
2024-02-08 11:53:12 +0000

    GOA of Top25 & Top100 human genes

--- FunctionalAnalysis/00.Installs.R
+++ FunctionalAnalysis/00.Installs.R
@@ -1,5 +1,12 @@
 # Installs needed to run the R code in this folder
 
 install.packages('biocmanager')
+
 BiocManager::install('clusterProfiler')
-BiocManager::install('RDAVIDWebService')
+BiocManager::install('RDAVIDWebService', lib ="C:/Users/antun/R/win-library/4.3")  # not available for Bioconductor 3.18
+BiocManager::install('BiocVersion',lib ="C:/Users/antun/R/win-library/4.3") 
+BiocManager::install('topGO',lib ="C:/Users/antun/R/win-library/4.3")
+BiocManager::install('org.Hs.eg.db',lib ="C:/Users/antun/R/win-library/4.3")
+BiocManager::install('GOstats',lib ="C:/Users/antun/R/win-library/4.3")
+BiocManager::install('AnnotationDbi',lib ="C:/Users/antun/R/win-library/4.3")
+BiocManager::install('DOSE',lib ="C:/Users/antun/R/win-library/4.3") 
---
+++ FunctionalAnalysis/GO_Analysis/GO_Analaysis_Top25.R
@@ -0,0 +1,117 @@
+#### LOAD LIBRARIES ####
+library(clusterProfiler)
+library(org.Hs.eg.db)
+library(ggplot2)
+
+#### LOAD THE DATA ####
+
+# List of file names
+file_names <- c("top_25_cpgs_clock_1.csv", "top_25_cpgs_clock_2.csv", "top_25_cpgs_clock_3.csv", "top_25_cpgs_overlap_1_2_3.csv", "top_25_cpgs_overlap_2_3.csv")
+
+# List to store data frames
+cpgs_data_list <- list()
+
+# Loop through each file and read into a data frame
+for (file_name in file_names) {
+  # Create the file path
+  csv_file_path <- file.path("FunctionalAnalysis", file_name)
+  
+  # Read the CSV file into a data frame and add it to the list
+  cpgs_data_list[[file_name]] <- read.csv(csv_file_path)
+}
+
+# Access the data frames from the list
+clock_1_df <- cpgs_data_list[["top_25_cpgs_clock_1.csv"]]
+clock_2_df <- cpgs_data_list[["top_25_cpgs_clock_2.csv"]]
+clock_3_df <- cpgs_data_list[["top_25_cpgs_clock_3.csv"]]
+overlap_1_2_3_df <- cpgs_data_list[["top_25_cpgs_overlap_1_2_3.csv"]]
+overlap_2_3_df <- cpgs_data_list[["top_25_cpgs_overlap_2_3.csv"]]
+
+
+
+#### GENE ONTOLOGY ENRICHMENT ANALYSIS ####
+
+
+# Create a list of data frames for the gene sets
+gene_set_data_list <- list(
+  "clock_1" = clock_1_df,
+  "clock_2" = clock_2_df,
+  "clock_3" = clock_3_df,
+  "overlap_1_2_3" = overlap_1_2_3_df,
+  "overlap_2_3" = overlap_2_3_df
+)
+
+# Create a list to store the results of the gene set enrichment analysis
+GOA_results_list <- list()
+
+# Loop through each data frame and perform gene set enrichment analysis
+for (gene_set_name in names(gene_set_data_list)) {
+  # Get the data frame for the gene set
+  gene_set_df <- gene_set_data_list[[gene_set_name]]
+  
+  # Extract the gene symbols from the data frame
+  gene_symbols <- gene_set_df$Gene # symbol
+  gene_entrez <- gene_set_df$ENTREZID
+  
+  # Perform gene set enrichment analysis using the DOSE package 
+  GOA_results <- enrichGO(gene = gene_symbols, 
+                              OrgDb = org.Hs.eg.db,
+                              keyType = "SYMBOL",
+                              ont = "BP", # GO is structured into three main ontologies: Biological Process (BP), Molecular Function (MF), and Cellular Component (CC).
+                              pAdjustMethod = "BH", # Benjamini-Hochberg (BH) coerrection
+                              pvalueCutoff = 0.05,
+                              qvalueCutoff = 0.05) # use 0.2 or 0.05 depending on the number of genes
+  
+  
+  # Add the results to the list
+  GOA_results_list[[gene_set_name]] <- GOA_results
+}
+
+# Access the results of the gene set enrichment analysis
+clock_1_GOA_results <- GOA_results_list[["clock_1"]]
+clock_2_GOA_results <- GOA_results_list[["clock_2"]]
+clock_3_GOA_results <- GOA_results_list[["clock_3"]]
+overlap_1_2_3_GOA_results <- GOA_results_list[["overlap_1_2_3"]]
+overlap_2_3_GOA_results <- GOA_results_list[["overlap_2_3"]]
+
+# Print the results
+print(clock_1_GOA_results)
+print(clock_2_GOA_results)
+print(clock_3_GOA_results)
+print(overlap_1_2_3_GOA_results)
+print(overlap_2_3_GOA_results)
+
+
+# Save the results to CSV files
+write.csv(clock_1_GOA_results, file = "FunctionalAnalysis/GO_Analysis/clock_1_GOA_25_results.csv")
+write.csv(clock_2_GOA_results, file = "FunctionalAnalysis/GO_Analysis/clock_2_GOA_25_results.csv")
+write.csv(clock_3_GOA_results, file = "FunctionalAnalysis/GO_Analysis/clock_3_GOA_25_results.csv")
+write.csv(overlap_1_2_3_GOA_results, file = "FunctionalAnalysis/GO_Analysis/overlap_1_2_3_GOA_25_results.csv")
+write.csv(overlap_2_3_GOA_results, file = "FunctionalAnalysis/GO_Analysis/overlap_2_3_GOA_25_results.csv")
+
+
+# Visualize the results
+# dotplot(clock_1_GOA_results, showCategory = 10)  # empty plot
+dotplot(clock_2_GOA_results, showCategory = 10, title = "Top 10 GO terms for clock 2")
+dotplot(clock_3_GOA_results, showCategory = 15, font=7, title = "Top 15 GO terms for clock 3")
+dotplot(overlap_1_2_3_GOA_results, showCategory = 15, title = "Top 15 GO terms for overlap of clocks 1, 2 and 3")
+dotplot(overlap_2_3_GOA_results, showCategory = 15, font=7, title = "Top 15 GO terms for overlap 2 and 3")
+
+
+# Export the visualizations to PNG files
+# ggsave("FunctionalAnalysis/GO_Analysis/clock_1_GOA_results.png")
+ggsave("FunctionalAnalysis/GO_Analysis/clock_2_GOA_25_results.png")
+ggsave("FunctionalAnalysis/GO_Analysis/clock_3_GOA_25_results.png")
+ggsave("FunctionalAnalysis/GO_Analysis/overlap_1_2_3_GOA_25_results.png")
+ggsave("FunctionalAnalysis/GO_Analysis/overlap_2_3_GOA_25_results.png")
+
+
+
+
+
+
+
+
+
+
+
