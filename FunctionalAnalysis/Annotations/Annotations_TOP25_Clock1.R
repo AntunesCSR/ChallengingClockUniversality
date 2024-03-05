74c202f238d40a4c08facf7ba61638df639a36b3
AntunesCSR <antunes.csr@gmail.com>
2024-03-05 17:03:12 +0000

    Annotations R script updated - loop added

--- FunctionalAnalysis/Annotations/Annotations_TOP25_Clock1.R
+++ FunctionalAnalysis/Annotations/Annotations_TOP25_Clock1.R
@@ -17,7 +17,10 @@ library(org.Hs.eg.db)
 library(KEGGREST)
 library(KEGGgraph)
 library(AnnotationHub)
+library(AnnotationDbi)
 library(jsonlite)
+library(clusterProfiler)  # For pathway enrichment analysis
+library(igraph)  # For network analysis
 
 #### LOAD THE DATA ####
 
@@ -47,7 +50,7 @@ top25_ov2_3_e <- top25_ov2_3$ENTREZID # Empty. WHY??????? T^T
 #### FUNCTIONS ####
 
 
-# Function to retrieve protein-protein interaction data from BioGRI
+# Function to retrieve protein-protein interaction data from BioGRID
 retrieve_ppi_data_biogrid <- function(gene_list) {
   api_key <- "e250241e4295ebc3a4aa90057f51cc3d"
   url <- paste0("https://webservice.thebiogrid.org/interactions/?accesskey=", api_key,
@@ -88,7 +91,7 @@ retrieve_ppi_data_biogrid <- function(gene_list) {
 retrieve_annotations_entrez <- function(gene_list) {
   entrez <- useMart("ensembl", dataset = "hsapiens_gene_ensembl")
   entrez_annotations_data <- getBM(attributes = c("entrezgene_id", "external_gene_name", "description"), # external_gene_name correct
-                       filters = "external_gene_name", # To use the EntrezID, otherwise "external_gene_name" for symbol
+                       filters = "entrezgene_id", # To use the EntrezID, otherwise "external_gene_name" for symbol
                        values = gene_list,
                        mart = entrez)
   return(entrez_annotations_data)
@@ -121,157 +124,61 @@ retrieve_pathway_data_kegg <- function(gene_list) {
 }
 
 
-# Function to retrieve protein-protein interaction data from STRING          ############# FIX THIS ONE
-retrieve_ppi_data_string <- function(gene_list) {
-  string_db <- STRINGdb$new(version = "11", score_threshold = 700)
-  string_ppi_data <- string_db$get_interactions(gene_list)
-  return(string_ppi_data)
-}
 
 
-############# FIX FROM HERE
-# Function to clean and combine gene annotations
-clean_and_combine_annotations <- function(source_list) {
-  # Merge data frames based on the common gene identifier
-  combined_annotations <- Reduce(function(df1, df2) {
-    merge(df1, df2, by = "common_gene_identifier", all = TRUE)   #change the common_gene_identifier to the common column name
-    
-  }, annotation_list)
-  
-  # Combine columns and handle missing values
-  combined_annotations <- combined_annotations %>%
-    mutate(ensembl_gene_id = coalesce(ensembl_gene_id.x, ensembl_gene_id.y),
-           description = coalesce(description.x, description.y)) %>%
-    select(common_gene_identifier, ensembl_gene_id, description)
-  
-  return(combined_annotations)
-}
-
-
-
-#### RETRIEVE ANNOTATIONS ####
+#### RETRIEVE ANNOTATIONS - NO LOOOP - JSUT TESTING ####
 
 # Define the gene lists
-gene_lists_s <- list('ANK1')
-gene_lists_e <- list('268') 
+gene_lists_s <- list('ANK1', 'RPS6KA3', 'RPS6KA3', 'RPS6KA3', 'RPS6KA3')
+gene_lists_e <- list('268', '6195', '6195', '6195', '6195') 
 
-# 1.1 Retrieve Gene Annotations from BioGrid
-gene_annotations_biogrid <- lapply(gene_lists_s, retrieve_ppi_data_biogrid)
-gene_ppi_biogrid_edge_list <- lapply(gene_lists_s, retrieve_ppi_data_biogrid_edge_list)
-print(gene_annotations_biogrid)
-print(gene_ppi_biogrid_edge_list)
+# 1.1 Retrieve PPI data from BioGrid
+ppi_data <- lapply(gene_lists_s, retrieve_ppi_data_biogrid_edge_list)
+print(ppi_data)
 
 
-# # 1.2. Retrieve Gene Annotations from DAVID
-# gene_annotations_david <- lapply(gene_lists, retrieve_functional_classification_david)
-# print(gene_annotations_david)
+# 1.2. Retrieve Gene Annotations from ENTREZ - NCBI GENE ID's
+annotations_data <- lapply(gene_lists_e, retrieve_annotations_entrez)
+print(annotations_data)
 
 
-# 1.3. Retrieve Gene Annotations from Ensembl
-gene_annotations_ensembl <- lapply(gene_lists, retrieve_annotations_ensembl)
-print(gene_annotations_ensembl)
-
-# 1.4. Retrieve Gene Annotations from ENTREZ
-gene_annotations_entrez <- lapply(gene_lists, retrieve_annotations_entrez)
-print(gene_annotations_entrez)
+# 1.3. Retrieve pathway from KEGG - NEEDS NCBI GENE ID's
+pathway_data <- lapply(gene_lists_e, retrieve_pathway_data_kegg)
+print(pathway_data)
 
 
-# 1.5. Retrieve Gene Annotations from KEGG
-gene_pathway_kegg <- lapply(gene_lists_e, retrieve_pathway_data_kegg)
-print(gene_pathway_kegg)
+#### LOOP ###
 
+# Define the gene lists
+gene_lists_s <- list('ANK1', 'RPS6KA3', 'RPS6KA3', 'RPS6KA3', 'RPS6KA3')
+gene_lists_e <- list('268', '6195', '6195', '6195', '6195')
 
-# 1.6. Retrieve Gene Annotations from STRING
-gene_annotations_string <- lapply(gene_lists, retrieve_ppi_data_string)  
-print(gene_annotations_string)   
+# Initialize empty lists to store results for each gene list
+ppi_data_list <- list()
+annotations_data_list <- list()
+pathway_data_list <- list()
 
-# 1.7. Retrieve Gene Annotations from UniProt
-gene_annotations_uniprot <- lapply(gene_lists, retrieve_annotations_uniprot)
-print(gene_annotations_uniprot)
+# Iterate over each gene list
+for (i in seq_along(gene_lists_e)) {
+  # 1.1 Retrieve PPI data from BioGrid
+  ppi_data <- retrieve_ppi_data_biogrid_edge_list(gene_lists_s[[i]])
+  ppi_data_list[[i]] <- ppi_data
+  
+  # 1.2 Retrieve Gene Annotations from ENTREZ - NCBI GENE ID's
+  annotations_data <- retrieve_annotations_entrez(gene_lists_e[[i]])
+  annotations_data_list[[i]] <- annotations_data
+  
+  # 1.3 Retrieve pathway from KEGG - NEEDS NCBI GENE ID's
+  pathway_data <- retrieve_pathway_data_kegg(gene_lists_e[[i]])
+  pathway_data_list[[i]] <- pathway_data
+}
 
-
-# 2. Define the sources. Here we can remove any unwanted source from the list so it doesn't appear
-# in the final combined annotations.
-source_list <- list(biogrid_ppi_data, # BioGrid PPI data
-                    david_enrichment_data, # DAVID functional enrichment data
-                    ensembl_annotations_data, # ENSEMBL gene annotations
-                    entrez_annotations_data, # NCBI ENTREZ gene annotations
-                    kegg_pathway_data, # KEGG pathway data
-                    string_ppi_data, # STRING PPI data
-                    uniprot_annotations_data # UniProt gene annotations
-)
-
-
-
-# 3. Clean and Combine Gene Annotations
-final_combined_annotations <- clean_and_combine_annotations(source_list)
-
-
-# 4. Print the combined annotations
-print("Final Combined Gene Annotations:")
-print(final_combined_annotations)
-
-
-# 5. Save the combined annotations to a file
-write.csv(final_combined_annotations, "combined_annotations.csv", row.names = FALSE)
+# Print the results for each gene list
+print(ppi_data_list)
+print(annotations_data_list)
+print(pathway_data_list)
 
 
 
-# #### RETRIEVE ANNOTATIONS - LOOP####
-# 
-# # Define the gene lists for each clock separately
-# gene_lists <- list(top25_clk1_s) #, top25_clk2_s, top25_clk3_s, top25_ov1_2_3_s, top25_ov2_3_s)
-# 
-# # Loop over each clock separately
-# for (i in seq_along(gene_lists)) {
-#   
-#   # Retrieve Gene Annotations from BioGrid 
-#   gene_annotations_biogrid <- retrieve_ppi_data_biogrid(gene_lists[[i]])
-#   print(gene_annotations_biogrid)
-#   
-#   # Retrieve Gene Annotations from DAVID
-#   gene_annotations_david <- retrieve_functional_enrichment_david(gene_lists[[i]])
-#   print(gene_annotations_david)
-#   
-#   # Retrieve Gene Annotations from Ensembl 
-#   gene_annotations_ensembl <- retrieve_annotations_ensembl(gene_lists[[i]])
-#   print(gene_annotations_ensembl)
-#   
-#   # Retrieve Gene Annotations from ENTREZ
-#   gene_annotations_entrez <- retrieve_annotations_entrez(gene_lists[[i]])
-#   print(gene_annotations_entrez)
-#   
-#   # Retrieve Gene Annotations from KEGG
-#   gene_annotations_kegg <- retrieve_pathway_data_kegg(gene_lists[[i]])
-#   print(gene_annotations_kegg)
-#   
-#   # Retrieve Gene Annotations from STRING
-#   gene_annotations_string <- retrieve_ppi_data_string(gene_lists[[i]])
-#   print(gene_annotations_string)
-#   
-#   # Retrieve Gene Annotations from UniProt
-#   gene_annotations_uniprot <- retrieve_annotations_uniprot(gene_lists[[i]])
-#   print(gene_annotations_uniprot)
-#   
-#   # Define the sources for each clock
-#   source_list <- list(
-#     biogrid_ppi_data = gene_annotations_biogrid,
-#     david_enrichment_data = gene_annotations_david,
-#     ensembl_annotations_data = gene_annotations_ensembl,
-#     entrez_annotations_data = gene_annotations_entrez,
-#     kegg_pathway_data = gene_annotations_kegg,
-#     string_ppi_data = gene_annotations_string,
-#     uniprot_annotations_data = gene_annotations_uniprot
-#   )
-#   
-#   # Clean and Combine Gene Annotations
-#   final_combined_annotations <- clean_and_combine_annotations(source_list)
-#   
-#   # Print the combined annotations
-#   print("Final Combined Gene Annotations:")
-#   print(final_combined_annotations)
-#   
-#   # Save the combined annotations to a file with a name reflecting the clock being processed
-#   write.csv(final_combined_annotations, paste0("combined_annotations_clock", i, ".csv"), row.names = FALSE)
-# }
-# 
+
+
